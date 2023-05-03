2ba6058f03503b028828add08c5a0d8b2cb4f41d
Dima Usov <dmitry.usov@yahoo.com>
2023-05-03 22:34:27 +0300

    fix: address

--- server/scripts/generate.js
+++ server/scripts/generate.js
@@ -4,7 +4,7 @@ const { keccak256 } = require('ethereum-cryptography/keccak');
 
 const privateKey = secp256k1.utils.randomPrivateKey();
 const publicKey = secp256k1.getPublicKey(privateKey);
-const address = toHex(keccak256(publicKey.slice(1).slice(-20)));
+const address = toHex(keccak256(publicKey.slice(1)).slice(-20));
 console.log('Private Key', toHex(privateKey));
 console.log('Public Key', toHex(publicKey));
 console.log(`Address 0x${address}`);
